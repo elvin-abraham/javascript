3fc45348d46b49eb6728afe0ee1b120957655294
first-repository <elvinmabraham@gmail.com>
2025-02-14 23:17:50 +0530

    task 3

---
+++ task 3.js
@@ -0,0 +1,9 @@
+// Simple Interest
+
+let P=parseFloat(prompt("Enter Principle "));
+let n=parseInt(prompt("Enter Period"));
+let R=parseFloat(prompt("Enter Rate"));
+
+let SI=(P*R*n)/100;
+
+console.log("Simpl Interest is: "+SI);
